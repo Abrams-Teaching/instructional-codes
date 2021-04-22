6246287ac1d516b631b60b1e6be44a715d3a81a8
Cameron Abrams <cfa22@drexel.edu>
2021-04-22 15:46:48 -0400

    rdf

--- originals/plot_rdf.py
+++ originals/plot_rdf.py
@@ -3,13 +3,14 @@ import numpy as np
 import argparse as ap
 
 parser=ap.ArgumentParser()
-parser.add_argument('-i',type=str,default='rdf.dat',help='input file name')
+parser.add_argument('-i',type=str,default=['rdf.dat'],action='append',help='input file name')
 parser.add_argument('-o',type=str,default='rdf.png',help='output file name')
 args=parser.parse_args()
 
-r,g=np.loadtxt(args.i,unpack=True)
-fig,ax=plt.subplots(1,1,figsize=(4,3))
+fig,ax=plt.subplots(1,1,figsize=(6,5))
 ax.set_xlabel("r ($\sigma$)")
 ax.set_ylabel("g(r)")
-ax.plot(r,g,'b-')
+for f in args.i:
+    r,g=np.loadtxt(f,unpack=True)
+    ax.plot(r,g)
 plt.savefig(args.o,bbox_inches='tight')
