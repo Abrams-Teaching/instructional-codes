99f84338a1780b40315be00ea99537f3fd248ce1
Cameron Abrams <cfa22@drexel.edu>
2021-05-28 08:00:54 -0400

    mclj update

--- originals/mclj.c
+++ originals/mclj.c
@@ -25,18 +25,16 @@
 #include <math.h>
 #include <gsl/gsl_rng.h>
 
-/* An N^2 algorithm for computing the total energy.  The virial
-   is also computed and returned in *vir. */
-double total_e ( double * rx, double * ry, double * rz, int N, double L,
+/* energy of particle i */
+double e_i ( int i, double * rx, double * ry, double * rz, int N, double L,
 		 double rc2, int tailcorr, double ecor, 
-		 int shift, double ecut, double * vir ) {
-  int i,j;
+		 int shift, double ecut, double * vir, int i0 ) {
+  int j;
   double dx, dy, dz, r2, r6i;
   double e = 0.0, hL=L/2.0;
-
   *vir=0.0;
-  for (i=0;i<(N-1);i++) {
-    for (j=i+1;j<N;j++) {
+  for (j=i0;j<N;j++) {
+    if (i!=j) {
       dx  = rx[i]-rx[j];
       dy  = ry[i]-ry[j];
       dz  = rz[i]-rz[j];
@@ -57,7 +55,22 @@ double total_e ( double * rx, double * ry, double * rz, int N, double L,
       }
     }
   }
-  return e+(tailcorr?(N*ecor):0.0);
+  return e+(tailcorr?ecor:0.0);
+}
+/* An N^2 algorithm for computing the total energy.  The virial
+   is also computed and returned in *vir. */
+double total_e ( double * rx, double * ry, double * rz, int N, double L,
+		 double rc2, int tailcorr, double ecor, 
+		 int shift, double ecut, double * vir ) {
+  int i;
+  double tvir;
+  double e = 0.0;
+  *vir=0.0;
+  for (i=0;i<N-1;i++) {
+    e    += e_i(i,rx,ry,rz,N,L,rc2,tailcorr,ecor,shift,ecut,&tvir,i+1);
+    *vir += tvir;
+  }
+  return e;
 }
 
 /* Writes configuration in XYZ format */
@@ -104,18 +117,22 @@ enum {XYZ, NONE};
 int main ( int argc, char * argv[] ) {
 
   double * rx, * ry, * rz;
-  int N=216,c,a,p;
+  int N=216,c,a;
   double L=0.0;
   double rho=0.5, T=1.0, rc2 = 3.5, vir, vir_old, vir_sum, pcor, V;
   double E_new, E_old, esum, rr3, ecor, ecut;
+  double ei_old, ei_new, delta_e;
+  double ivir_old, ivir_new, delta_vir;
   double dr=0.2,dx,dy,dz;
   double rxold,ryold,rzold;
+  double beta;
   int i,j;
   int nCycles = 10, nSamp, nEq=1000;
   int nAcc;
   int short_out=0;
   int shift=0;
   int tailcorr=1;
+  int prog=0;
 
   gsl_rng * r = gsl_rng_alloc(gsl_rng_mt19937);
   unsigned long int Seed = 23410981;
@@ -137,6 +154,7 @@ int main ( int argc, char * argv[] ) {
     else if (!strcmp(argv[i],"-so")) short_out=1;
     else if (!strcmp(argv[i],"+tc")) tailcorr=0;
     else if (!strcmp(argv[i],"-sh")) shift=1;
+    else if (!strcmp(argv[i],"-prog")) prog = atoi(argv[++i]);
     else if (!strcmp(argv[i],"-s")) 
       Seed = (unsigned long)atoi(argv[++i]);
     else if (!strcmp(argv[i],"-traj_samp")) traj_samp = atoi(argv[++i]);
@@ -168,16 +186,16 @@ int main ( int argc, char * argv[] ) {
   /* Compute the *squared* cutoff, reusing the variable rc2 */
   rc2*=rc2;
 
-  /* For computational efficiency, use reciprocal T */
-  T = 1.0/T;
+  /* Compute beta */
+  beta = 1.0/T;
 
   /* compute box volume */
   V = L*L*L;
   
   /* Output some initial information */
   fprintf(stdout,"# NVT MC Simulation of a Lennard-Jones fluid\n");
-  fprintf(stdout,"# L = %.5lf; rho = %.5lf; N = %i; rc = %.5lf\n",
-	  L,rho,N,sqrt(rc2));
+  fprintf(stdout,"# T = %.2lf; rho = % .3lf; N = %i; L = %.5lf; rc = %.5lf\n",
+	  T,L,rho,N,sqrt(rc2));
   fprintf(stdout,"# nCycles %i, nEq %i, seed %lu, dR %.5lf\n",
 	  nCycles,nEq,Seed,dr);
   
@@ -207,59 +225,65 @@ int main ( int argc, char * argv[] ) {
   esum = 0.0;
   nSamp = 0;
   vir_sum = 0.0;
+  if (prog>0) {
+    printf("#LABEL cycle <e>/<n> p mu_ex\n");
+  }
   for (c=0;c<nCycles;c++) {
-    for (p=0;p<N;p++) {
-      /* Randomly select a particle */
-      i=(int)gsl_rng_uniform_int(r,N);
-      /* calculate displacement */
-      dx = dr*(0.5-gsl_rng_uniform(r));
-      dy = dr*(0.5-gsl_rng_uniform(r));
-      dz = dr*(0.5-gsl_rng_uniform(r));
-      //printf("%d %.6lf %.6lf %.6lf\n",i,dx,dy,dz);
-      /* Save the current position of particle i */
-      rxold=rx[i];
-      ryold=ry[i];
-      rzold=rz[i];
+    /* Randomly select a particle */
+    i=(int)gsl_rng_uniform_int(r,N);
+    ei_old=e_i(i,rx,ry,rz,N,L,rc2,tailcorr,ecor,
+               shift,ecut,&ivir_old,0);
+    /* calculate displacement */
+    dx = dr*(0.5-gsl_rng_uniform(r));
+    dy = dr*(0.5-gsl_rng_uniform(r));
+    dz = dr*(0.5-gsl_rng_uniform(r));
+    /* Save the current position of particle i */
+    rxold=rx[i];
+    ryold=ry[i];
+    rzold=rz[i];
 
-      /* Displace particle i */
-      rx[i]+=dx;
-      ry[i]+=dy;
-      rz[i]+=dz;
+    /* Displace particle i */
+    rx[i]+=dx;
+    ry[i]+=dy;
+    rz[i]+=dz;
 
-      /* Apply periodic boundary conditions */
-      if (rx[i]<0.0) rx[i]+=L;
-      if (rx[i]>L)   rx[i]-=L;
-      if (ry[i]<0.0) ry[i]+=L;
-      if (ry[i]>L)   ry[i]-=L;
-      if (rz[i]<0.0) rz[i]+=L;
-      if (rz[i]>L)   rz[i]-=L;
+    /* Apply periodic boundary conditions */
+    if (rx[i]<0.0) rx[i]+=L;
+    if (rx[i]>L)   rx[i]-=L;
+    if (ry[i]<0.0) ry[i]+=L;
+    if (ry[i]>L)   ry[i]-=L;
+    if (rz[i]<0.0) rz[i]+=L;
+    if (rz[i]>L)   rz[i]-=L;
 
-      /* Get the new energy */
-      E_new = total_e(rx,ry,rz,N,L,rc2,tailcorr,ecor,shift,ecut,&vir);
+    /* Get the change in enegy energy */
+    ei_new=e_i(i,rx,ry,rz,N,L,rc2,tailcorr,ecor,shift,ecut,&ivir_new,0);
+    delta_e=ei_new-ei_old;
+    delta_vir=ivir_new-ivir_old;
       
-      //exit(-1);
-      /* Conditionally accept... */
-      if (gsl_rng_uniform(r) < exp(-T*(E_new-E_old))) {
-        E_old=E_new;
-        vir_old=vir;
-        nAcc++;
-        //printf("%d %.5lf %.5lf %d\n",c,E_new,E_old,nAcc);
-      }
-      /* ... or reject the move; reassign the old positions */
-      else {
-        rx[i]=rxold;
-        ry[i]=ryold;
-        rz[i]=rzold;
-      }
-      /* Sample: default frequency is once per trial move; We must
-        include results of a move regardless of whether the move is
-        accepted or rejected. */
-      if (c>nEq) {
-        esum+=E_old;
-        vir_sum+=vir_old;
-        nSamp++;
-      }
+    /* Conditionally accept... */
+    if (gsl_rng_uniform(r) < exp(-beta*delta_e)) {
+      E_old+=delta_e;
+      vir_old+=delta_vir;
+      nAcc++;
     }
+    /* ... or reject the move; reassign the old positions */
+    else {
+      rx[i]=rxold;
+      ry[i]=ryold;
+      rz[i]=rzold;
+    }
+    /* Sample: default frequency is once per trial move; We must
+       include results of a move regardless of whether the move is
+       accepted or rejected. */
+    if (c>nEq) {
+      esum+=E_old;
+      vir_sum+=vir_old;
+      nSamp++;
+    }
+      if (prog>0&&!(c%prog)) {
+        printf("% 10i % .5f % .5f\n",c,esum/nSamp/N,vir_sum/3.0/V/nSamp+rho/beta);
+        fflush(stdout);
+      }
     if (traj_fn&&!(c%traj_samp)) {
       if (traj_out==XYZ) {
         fprintf(stdout,"# Trajectory snapshot at %i\n",c);fflush(stdout);
@@ -275,36 +299,36 @@ int main ( int argc, char * argv[] ) {
   if (short_out)
     fprintf(stdout,"%.6lf %.5lf %.5lf %.5lf\n",
 	    dr,((double)nAcc)/(N*nCycles),
-	    esum/nSamp/N,vir_sum/3.0/nSamp/V+rho*T+pcor);
+	    esum/nSamp/N,vir_sum/3.0/nSamp/V+rho/beta+pcor);
   else
-    fprintf(stdout,"NVT Metropolis Monte Carlo Simulation"
-	    " of the Lennard-Jones fluid.\n"
-	    "---------------------------------------------\n"
-	    "Number of particles:              %i\n"
-	    "Number of cycles:                 %i\n"
-	    "Cutoff radius:                    %8.5lf\n"
-	    "Maximum displacement:             %8.5lf\n"
-	    "Density:                          %8.5lf\n"
-	    "Temperature:                      %8.5lf\n"
-	    "Tail corrections applied?         %s\n"
-	    "Shifted potential?                %s\n"
-	    "Results:\n"
-	    "Potential energy tail correction: %8.5lf\n"
-	    "Pressure tail correction:         %8.5lf\n"
-	    "Potential energy shift at cutoff  %8.5lf\n"
-	    "Acceptance ratio:                 %8.5lf\n"
-	    "Energy/particle:                  %8.5lf\n"
-	    "Ideal gas pressure:               %8.5lf\n"
-	    "Virial:                           %8.5lf\n"
-	    "Total Pressure:                   %8.5lf\n"
-	    "Program ends.\n",
-	    N,nCycles,sqrt(rc2),dr,rho,1.0/T,
+    fprintf(stdout,"# NVT Metropolis Monte Carlo Simulation"
+	    "# of the Lennard-Jones fluid.\n"
+	    "# ---------------------------------------------\n"
+	    "# Number of particles:              %i\n"
+	    "# Number of cycles:                 %i\n"
+	    "# Cutoff radius:                    %8.5lf\n"
+	    "# Maximum displacement:             %8.5lf\n"
+	    "# Density:                          %8.5lf\n"
+	    "# Temperature:                      %8.5lf\n"
+	    "# Tail corrections applied?         %s\n"
+	    "# Shifted potential?                %s\n"
+	    "# Results:\n"
+	    "# Potential energy tail correction: %8.5lf\n"
+	    "# Pressure tail correction:         %8.5lf\n"
+	    "# Potential energy shift at cutoff  %8.5lf\n"
+	    "# Acceptance ratio:                 %8.5lf\n"
+	    "# Energy/particle:                  %8.5lf\n"
+	    "# Ideal gas pressure:               %8.5lf\n"
+	    "# Virial:                           %8.5lf\n"
+	    "# Total Pressure:                   %8.5lf\n"
+	    "# Program ends.\n",
+	    N,nCycles,sqrt(rc2),dr,rho,T,
 	    tailcorr?"Yes":"No",shift?"Yes":"No",
 	    ecor,pcor,ecut,
-	    ((double)nAcc)/(N*nCycles),
+	    ((double)nAcc)/nCycles,
 	    esum/nSamp/N,
-	    rho/T,vir_sum/3.0/nSamp/V,
-	    vir_sum/3.0/nSamp/V+rho/T+(tailcorr?pcor:0.0));
+	    rho/beta,vir_sum/3.0/nSamp/V,
+	    vir_sum/3.0/nSamp/V+rho/beta+(tailcorr?pcor:0.0));
   if (traj_fn) {
     fprintf(stdout,"Trajectory written to %s.\n",traj_fn);
   }
